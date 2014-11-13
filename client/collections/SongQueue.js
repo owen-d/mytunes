57e210799256e3e4e83f1198ef461a74de0e7762
owen-d <ow.diehl@gmail.com>
2014-11-12 20:53:51 -0800

    basic reqs met

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,26 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
-  }
+    this.on('ended', function(){
+      this.remove(this.at(0));
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('add', function(){
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function(song){
+      this.remove(song);
+    }, this);
+  },
+
+  playFirst: function(){
+    this.at(0).play();
+  }  
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,21 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+    this.collection.on('add remove', function(){
+      this.render();
+    }, this);
+    this.render();
   },
 
   render: function() {
-    return this.$el;
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
